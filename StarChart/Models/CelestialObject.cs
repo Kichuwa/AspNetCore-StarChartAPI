17cfd0d747b824fa779dc1b1f76e4cb6c72cf9c5
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2022-07-31 10:39:30 -0500

    Created ObjectModel for Celestial Objects

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id;
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name;
+        public int? OrbitedObjectId;
+        [NotMapped]
+        public List<CelestialObject> Sattelites;
+        public TimeSpan OrbitalPeriod;
+
+
+
+    }
+}
